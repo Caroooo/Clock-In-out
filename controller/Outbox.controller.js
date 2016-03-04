e7968fc3f0ff76fd80d2bd2680499c8c9b5ffff0
Caroooo <caroline.meusburger@students.fhv.at>
2016-03-04 17:38:53 +0100

    scrollable outbox

--- controller/Outbox.controller.js
+++ controller/Outbox.controller.js
@@ -3,37 +3,21 @@
  */
 
 sap.ui.define([
-    'jquery.sap.global',
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    'sap/ui/Device'
-], function (jQuery, Controller, JSONModel, Device) {
+    "sap/ui/model/json/JSONModel"
+], function ( Controller, JSONModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.wt.controller.Outbox", {
         metadata : {
             manifest: "json"
         },
         init : function () {
-            // call the init function of the parent
-            UIComponent.prototype.init.apply(this, arguments);
-             //set data model
-            var oData = {
-                recipient : {
-                    name : "World",
-                    width: (Device.system.phone) ? "50em" : "100em"
-                }
-            };
-            var oModel = new JSONModel();
-            this.setModel(oModel);
 
             // set explored app's demo model on this sample
             var oConfig = this.getMetadata().getConfig();
             var sNamespace = this.getMetadata().getManifestEntry("sap.app").id;
             var oOutboxModel = new JSONModel(jQuery.sap.getModulePath(sNamespace, oConfig.outboxLocal));
-            this.setModel(oOutboxModel, "outbox")
-
-
-
+            this.setModel(oOutboxModel, "outbox");
         }
 
     });
