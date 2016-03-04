1c197c343f4ae04bc3248edccb1598f1592cae48
Caroooo <caroline.meusburger@students.fhv.at>
2016-03-04 10:47:28 +0100

    bug fix view change

---
+++ controller/Main2.controller.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Caroline on 31.01.2016.
+ */
+
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.wt.controller.Main2", {
+
+
+
+        handlePressConfiguration: function(oEvent) {
+            var oItem = oEvent.getSource();
+            var oShell = this.getView().byId("myShell");
+            var bState = oShell.getShowPane();
+            oShell.setShowPane(!bState);
+          //  oItem.setShowMarker(!bState);
+            // oItem.setSelected(!bState);
+
+        },
+
+        onLogout : function(oEvent){
+            var oShell = this.getView().byId("myShell");
+            var bState = oShell.getShowPane();
+            oShell.setShowPane(!bState);
+
+
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("login");
+        }
+
+    });
+});
